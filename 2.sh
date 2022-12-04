c6b04f2833900a9ce16b36ad8ca6796f58426b5b
Ian Wright <ianwrightaw@gmail.com>
2022-12-04 01:13:19 -0500

    :shrug: :sigh:

--- 2.sh
+++ 2.sh
@@ -1,2 +1,4 @@
+# part 1
 paste -sd+ <(tr 'XYZ' 'ABC' < input | tr 'ABC' '123' | sed 's/\ //;s/\(.\)\1/\1+3/;s/21/1/;s/32/2/;s/13/3/;s/12/8/;s/23/9/;s/31/7/' | bc) | bc
+# part 2
 paste -sd+ <(cat input | sed -r 's/ //;s/([A-C])(Y)/\1\1/;s/AX/AC/;s/BX/BA/;s/CX/CB/;s/AZ/AB/;s/BZ/BC/;s/CZ/CA/' | tr 'ABC' '123' | sed 's/\(.\)\1/\1+3/;s/21/1/;s/32/2/;s/13/3/;s/12/8/;s/23/9/;s/31/7/' | bc) | bc

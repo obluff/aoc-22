fca43a4915334df8e8bc033529df772129082dd9
Ian Wright <ianwrightaw@gmail.com>
2022-12-06 22:27:44 -0800

    add print stmt

--- 7.py
+++ 7.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import re
 def load():
     with open("/home/obluff/Downloads/input") as inp:
         return [x for x in inp.read().split("\n") if x]
@@ -43,10 +44,13 @@ def calc(target, dirs, kids, files, file_size):
     curr_lvl = sum([int(file_size[_]) for _ in files[target]])
     if not kids[target]:
         return curr_lvl
-    return  curr_lvl + sum([c(kid, dirs, kids, files, file_size) for kid in kids[target]])
+    return  curr_lvl + sum([calc(kid, dirs, kids, files, file_size) for kid in kids[target]])
 
 inp = load()
 dirs, kids, files, file_size = parse(inp)
 data = dict([(_, calc(_, dirs, kids, files, file_size)) for _ in dirs])
+
+print(sum([_ for _ in data.values() if _ <= 100000]))
+
 delta = 70000000 - 30000000
-[x for x in sorted(data.items(), key=lambda x: x[1]) if data['home']-x[1]<=delta][0]
+print([x for x in sorted(data.items(), key=lambda x: x[1]) if data['home']-x[1]<=delta][0])
